dc36d6e1901743f0ab2e5f0177093218f340d62f
Leonardo Peixoto <ljpeixoto@gmail.com>
2023-03-01 08:45:04 -0300

    Change component to load Lessons from backend

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -3,114 +3,50 @@ import { ActivatedRoute } from '@angular/router';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { Lesson } from '../model/lesson';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 
 @Component({
-    selector: 'course',
-    templateUrl: './course.component.html',
-    styleUrls: ['./course.component.scss']
+  selector: 'course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit, AfterViewInit {
 
-    course:Course;
+  course: Course;
 
-    lessons: Lesson[] = [
-       {
-        id: 120,
-        'description': 'Introduction to Angular Material',
-        'duration': '4:17',
-        'seqNo': 1,
-        courseId: 11
-      },
-      {
-        id: 121,
-        'description': 'Navigation and Containers',
-        'duration': '6:37',
-        'seqNo': 2,
-        courseId: 11
-      },
-      {
-        id: 122,
-        'description': 'Data Tables',
-        'duration': '8:03',
-        'seqNo': 3,
-        courseId: 11
-      },
-      {
-        id: 123,
-        'description': 'Dialogs and Overlays',
-        'duration': '11:46',
-        'seqNo': 4,
-        courseId: 11
-      },
-      {
-        id: 124,
-        'description': 'Commonly used Form Controls',
-        'duration': '7:17',
-        'seqNo': 5,
-        courseId: 11
-      },
-      {
-        id: 125,
-        'description': 'Drag and Drop',
-        'duration': '8:16',
-        'seqNo': 6,
-        courseId: 11
-      },
-      {
-        id: 126,
-        'description': 'Responsive Design',
-        'duration': '7:28',
-        'seqNo': 7,
-        courseId: 11
-      },
-      {
-        id: 127,
-        'description': 'Tree Component',
-        'duration': '11:09',
-        'seqNo': 8,
-        courseId: 11
-      },
-      {
-        id: 128,
-        'description': 'Virtual Scrolling',
-        'duration': '3:44',
-        'seqNo': 9,
-        courseId: 11
-      },
-      {
-        id: 129,
-        'description': 'Custom Themes',
-        'duration': '8:55',
-        'seqNo': 10,
-        courseId: 11
-      },
-      {
-        id: 130,
-        'description': 'Changing Theme at Runtime',
-        'duration': '12:37',
-        'seqNo': 11,
-        courseId: 11
-      }
-    ];
+  lessons: Lesson[] = [];
 
-    displayedColumns = ['seqNo', 'description', 'duration'];
+  displayedColumns = ['seqNo', 'description', 'duration'];
 
-    constructor(private route: ActivatedRoute,
-                private coursesService: CoursesService) {
+  constructor(private route: ActivatedRoute,
+              private coursesService: CoursesService) {
 
-    }
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-        this.course = this.route.snapshot.data["course"];
+    this.course = this.route.snapshot.data['course'];
+
+    this.loadLessonsPage();
+
+  }
+
+  ngAfterViewInit() {
 
 
-    }
+  }
 
-    ngAfterViewInit() {
-
-
-    }
-
+  private loadLessonsPage() {
+    this.coursesService.findLessons(this.course.id, 'asc', 0, 3).pipe(
+      tap(lessons => this.lessons = lessons),
+      catchError(err => {
+        console.log('Error loading lessons', err);
+        alert('Error loading lessons');
+        return throwError(err);
+      })
+    )
+      .subscribe();
+  }
 }
